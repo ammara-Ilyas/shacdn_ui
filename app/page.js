09f66809b61a643d9b52931559bd0d8440c38d5b
ammara-Ilyas <ismarailyas545@gmail.com>
2024-05-09 11:07:38 +0500

    Shacdn Ui in Next.js

---
+++ app/page.js
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+import Nav from "./components/shacdnComponent/Nav";
+import Sheet from "./components/shacdnComponent/Sheet";
+const Home = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+      Menu
+      <Nav />
+      <h2>Sheet</h2>
+      <Sheet />
+    </>
+  );
+};
+
+export default Home;
